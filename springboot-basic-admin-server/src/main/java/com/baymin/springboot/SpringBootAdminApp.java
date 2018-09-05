0edc9d3b9e5f80ce8ed4e6a3e77e1b5d0482f638
ebaizon <zongwei.bai@ericsson.com>
2018-09-05 18:07:11 +0800

    add admin server module

---
+++ springboot-basic-admin-server/src/main/java/com/baymin/springboot/SpringBootAdminApp.java
@@ -0,0 +1,17 @@
+package com.baymin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by Baymin on 2017/4/9.
+ */
+@SpringBootApplication
+public class SpringBootAdminApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApp.class, args);
+    }
+
+}
